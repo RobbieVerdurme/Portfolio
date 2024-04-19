41eeb8b307ddc263aeb363f59d21eccb25da6225
Robbie Verdurme <robbie.verdurme@smartphoto.com>
2024-04-19 15:10:14 +0200

    style: change margin/padding navbar

--- src/pages/homepage/HomePage.tsx
+++ src/pages/homepage/HomePage.tsx
@@ -1,15 +1,5 @@
-import { HeroBanner } from "@components/herobanner/HeroBanner";
-import { MainLayout } from "@templates/mainlayout/MainLayout";
+import { MainLayout } from '@templates/mainlayout/MainLayout';
 
 export function HomePage() {
-  return (
-    <MainLayout>
-      <HeroBanner
-        title="Welcome"
-        subtitle="The goal of this portfolio is to give you an overview of my competences and skills."
-        backgroundImageUrl="/img/background.jpg"
-        fullScreen={true}
-      />
-    </MainLayout>
-  );
+  return <MainLayout></MainLayout>;
 }
--- src/templates/mainlayout/MainLayout.tsx
+++ src/templates/mainlayout/MainLayout.tsx
@@ -1,11 +1,18 @@
-import { NavBar } from "@components/layout/navbar/NavBar";
-import "./MainLayout.tsx.css";
+import { NavBar } from '@components/layout/navbar/NavBar';
+import './MainLayout.tsx.css';
+import { HeroBanner } from '@components/herobanner/HeroBanner';
 
-export function MainLayout({ children }: any) {
+export function MainLayout({ children }: { children?: React.ReactNode }) {
   return (
     <>
       <NavBar />
-      <main>{children}</main>
+      <HeroBanner
+        title="Welcome"
+        subtitle="The goal of this portfolio is to give you an overview of my competences and skills."
+        backgroundImageUrl="/img/background.jpg"
+        fullScreen={true}
+      />
+      <main className="">{children}</main>
     </>
   );
 }
