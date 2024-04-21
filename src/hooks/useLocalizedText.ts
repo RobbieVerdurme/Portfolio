e76c060e70bfb8b9efc9259c24400b40f15ba05f
RobbieVerdurme <robbievrdrm@gmail.com>
2024-04-21 17:37:18 +0200

    feat: add projects to page

--- src/hooks/useLocalizedText.ts
+++ src/hooks/useLocalizedText.ts
@@ -3,11 +3,11 @@ import { useTranslation } from 'react-i18next';
 export default function useLocalizedText(): {
   currentLanguage: string;
   supportedLanguages: readonly string[];
-  translate: (key: string) => string;
+  translate: <T extends string | object>(key: string) => T;
   changeLanguage: (newLanguage: string) => void;
 } {
   const { t, i18n } = useTranslation();
-  const translate = (key: string) => t(key);
+  const translate = <T>(key: string) => t(key, { returnObjects: true }) as T;
   const changeLanguage = (newLanguage: string) => {
     i18n.changeLanguage(newLanguage);
   };
--- src/pages/projectspage/ProjectsPage.tsx
+++ src/pages/projectspage/ProjectsPage.tsx
@@ -1,21 +1,42 @@
-import Card from '@components/card/Card';
 import { ContentLayout } from '@templates/contentLayout/ContentLayout';
+import { Project } from '@customTypes/Project';
+import { Button } from '@elements/button/Button';
+import useLocalizedText from '@hooks/useLocalizedText';
+import Card from '@components/card/Card';
 import './ProjectsPage.tsx.css';
 
 export default function ProjectsPage() {
+  const { translate } = useLocalizedText();
+  const projects = translate<Project[]>('profile.projects');
+  const readMore = translate<string>('projectdetail.readmore');
+
   return (
     <ContentLayout title="Projects">
       <div className="card-list">
-        <Card>
-          <Card.Body>
-            <Card.Title>hihi</Card.Title>
-            <Card.Text>
-              some longer text then i expected on such a small card on a small screen
-            </Card.Text>
-          </Card.Body>
-          <Card.Img src="/img/background.jpg" />
-        </Card>
+        {projects &&
+          projects.map((project) => (
+            <ProjectCard project={project} readMore={readMore} key={project.name}></ProjectCard>
+          ))}
       </div>
     </ContentLayout>
+  );
+}
+
+function ProjectCard({ project, readMore }: { project: Project; readMore: string }) {
+  return (
+    <Card>
+      <Card.Body className={project.img && 'md-layout-60'}>
+        <Card.Title>{project.name}</Card.Title>
+        <Card.Text>{project.description}</Card.Text>
+        <Button>{readMore.toUpperCase()}</Button>
+      </Card.Body>
+      {project.img && (
+        <div className="project-img-container md-layout-40">
+          {project.img.map((img) => (
+            <Card.Img src={img} key={img} />
+          ))}
+        </div>
+      )}
+    </Card>
   );
 }
