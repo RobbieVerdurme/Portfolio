d811df7ffcef2e3fe1cdd51b24f6be6939c87e6f
Robbie Verdurme <robbie.verdurme@smartphoto.com>
2024-04-24 22:27:44 +0200

    fix: css classes of layout file

--- src/pages/homepage/HomePage.tsx
+++ src/pages/homepage/HomePage.tsx
@@ -2,12 +2,13 @@ import { PathConstants } from '@constants/PathConstants';
 import { Button } from '@elements/button/Button';
 import useLocalizedText from '@hooks/useLocalizedText';
 import { HeroLayout } from '@templates/heroLayout/HeroLayout';
+import './HomePage.tsx.css';
 
 export function HomePage() {
   const { translate } = useLocalizedText();
   return (
     <HeroLayout>
-      <main>
+      <main className="container">
         <h1>{translate<string>('home.title')}</h1>
         <p>{translate<string>('home.introduction')}</p>
         <Button relativePageUrl={PathConstants.ABOUT}>
--- src/templates/contentLayout/ContentLayout.tsx
+++ src/templates/contentLayout/ContentLayout.tsx
@@ -1,14 +1,14 @@
 import { NavBar } from '@components/layout/navbar/NavBar';
-import './ContentLayout.tsx.css';
 import { HeroBanner } from '@components/herobanner/HeroBanner';
 import { Footer } from '@components/layout/footer/Footer';
+import './ContentLayout.tsx.css';
 
 export function ContentLayout({ children, title }: { children?: React.ReactNode; title?: string }) {
   return (
     <>
       <NavBar />
       <HeroBanner backgroundImageUrl="/img/background.jpg" bannerStyle="small" />
-      <main className="elevated">
+      <main className="elevated main-container">
         <div className="elevated-container">
           {title && (
             <div className="elevated-container-title">
