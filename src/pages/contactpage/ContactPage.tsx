dd20134f2630592115e391b705b3ff4b8d098e6d
Robbie Verdurme <robbie.verdurme@smartphoto.com>
2024-04-26 11:17:15 +0200

    feat: contact page

--- src/pages/contactpage/ContactPage.tsx
+++ src/pages/contactpage/ContactPage.tsx
@@ -1,5 +1,29 @@
-import { ContentLayout } from '@templates/contentLayout/ContentLayout';
+import Card from '@components/card/Card';
+import Form from '@elements/form/Form';
+import useLocalizedText from '@hooks/useLocalizedText';
+import { HeroLayout } from '@templates/heroLayout/HeroLayout';
+import './ContactPage.tsx.css';
 
 export default function ContactPage() {
-  return <ContentLayout></ContentLayout>;
+  const { translate } = useLocalizedText();
+  return (
+    <HeroLayout>
+      <Card alignRight={false}>
+        <Card.Title>
+          <h1 className="content-card-title">{translate<string>('contact.title')}</h1>
+        </Card.Title>
+        <Card.Body>
+          <p>{translate<string>('contact.description')}</p>
+          <Form action="https://formspree.io/xoqnokzd" method="POST">
+            <Form.Group>
+              <Form.Input label={translate<string>('contact.nameForm')} name="name" />
+              <Form.Input label={translate<string>('contact.emailForm')} name="email" />
+            </Form.Group>
+            <Form.TextArea label={translate<string>('contact.messageForm')} name="message" />
+            <Form.SubmitButton>{translate<string>('contact.sendFrom')}</Form.SubmitButton>
+          </Form>
+        </Card.Body>
+      </Card>
+    </HeroLayout>
+  );
 }
