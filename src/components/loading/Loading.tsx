1a4697cf4c21721acb527bb44de248572dff10c4
Robbie Verdurme <robbie.verdurme@smartphoto.com>
2024-04-26 21:53:43 +0200

    feat: add loading component

---
+++ src/components/loading/Loading.tsx
@@ -0,0 +1,12 @@
+import './Loading.tsx.css';
+
+export function Loading() {
+  return (
+    <div className="lds-ellipsis">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+}
