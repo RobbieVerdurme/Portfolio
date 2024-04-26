54944e773f8dca98722098bfbe4d16136b2174a0
Robbie Verdurme <robbie.verdurme@smartphoto.com>
2024-04-26 21:53:58 +0200

    feat: add loading for iframe

--- src/elements/iframe/IFrame.tsx
+++ src/elements/iframe/IFrame.tsx
@@ -1,9 +1,24 @@
+import { useState } from 'react';
 import './IFrame.tsx.css';
+import { Loading } from '@components/loading/Loading';
 
 interface IFrameProps extends React.IframeHTMLAttributes<HTMLIFrameElement> {
   variant?: 'small' | 'normal' | 'large';
 }
 export function IFrame({ variant = 'normal', ...props }: IFrameProps) {
+  const [isLoading, SetIsLoading] = useState(true);
+
   props.className = `iframe iframe-${variant} ${props.className ? props.className : ''}`;
-  return <iframe {...props} frameBorder={0} />;
+  return (
+    <>
+      {isLoading ? <Loading /> : <></>}
+      <iframe
+        {...props}
+        frameBorder={0}
+        style={{ opacity: isLoading ? '0' : '1' }}
+        loading="lazy"
+        onLoad={() => SetIsLoading(false)}
+      />
+    </>
+  );
 }
